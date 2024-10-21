280ce19cd46abcf982255d6c2c382ca3d435e1f8
Doud10 <ndongnnangdesire10@gmail.com>
2024-10-21 10:13:32 +0000

    kkkkkkk

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,11 +1,12 @@
 #include <stdio.h>
-cat <<EOF > 4-puts.c
+
+
 #include <stdio.h>
 int main(void) {
-    puts("Programming is like building a multilingual puzzle");
+    puts("\"Programming is like building a multilingual puzzle");
     return 0;
 }
-EOF
 
-gcc -o helloworld 4-puts.c -Wall -Werror -Wextra -pedantic -std=gnu89
+
+
 
