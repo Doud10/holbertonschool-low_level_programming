4e56dfef2a4a4e188fb4a42ba35dcad8f552b94e
Doud10 <ndongnnangdesire10@gmail.com>
2024-10-22 09:47:04 +0000

    faq

--- variables_if_else_while/8-print_base16.c
+++ variables_if_else_while/8-print_base16.c
@@ -9,19 +9,15 @@
  */
 int main(void)
 {
-    int i;
-
-    for (i = 0; i < 10; i++)  // Loop to print 0-9
-    {
-        putchar(i + '0');  // Convert integer to character
-    }
-
-    for (i = 10; i < 16; i++)  // Loop to print a-f
-    {
-        putchar(i - 10 + 'a');  // Convert integer to corresponding hex character
-    }
-
-    putchar('\n');  // Print a new line
-
-    return (0);  // Return 0 to indicate successful execution
+int i;
+for (i = 0; i < 10; i++)
+{
+putchar(i + '0');
 }
+for (i = 10; i < 16; i++)
+{
+putchar(i - 10 + 'a');
+}
+putchar('\n');
+return (0);
+{
