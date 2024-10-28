eb3bb296fd6dc64ac8061b313893fc9a2b61d539
Doud10 <ndongnnangdesire10@gmail.com>
2024-10-28 16:48:10 +0000

    blate

--- more_functions_nested_loops/5-more_numbers.c
+++ more_functions_nested_loops/5-more_numbers.c
@@ -1,22 +1,20 @@
 #include "main.h"
-#include <stdio.h> 
 /**
  * print_numbers_ten_times - Prints the numbers from 0 to 14, 10 times.
  */
 void print_numbers_ten_times(void)
 {
 int i, j;
-for (j = 0; j < 10; j++)
+for (j = 0; j < 10; j++) 
 {
-for (i = 0; i <= 14; i++)
+for (i = 0; i <= 14; i++) 
 {
-_putchar(i + '0')
-if (i > 9)
+if (i > 9) 
 {
-_putchar((i / 10) + '0')
-putchar((i % 10) + '0')
+_putchar((i / 10) + '0'); 
 }
+_putchar((i % 10) + '0'); 
 }
-_putchar('\n')
+_putchar('\n');
 }
 }
