3cc7a9458d02139a2611a47706408eba4350c3d9
Doud10 <ndongnnangdesire10@gmail.com>
2024-10-29 13:49:28 +0000

    rt

--- pointers_arrays_strings/3-puts.c
+++ pointers_arrays_strings/3-puts.c
@@ -3,7 +3,7 @@
  * print_string - Prints a string followed by a new line.
  * @str: Pointer to the string to be printed.
  */
-void print_string(char *str)
+void _puts(char *str)
 {
 while (*str != '\0')
 {
